417ed22dd4205be2c9adca0025c9aa48afc74583
Chris Scott <christocracy@gmail.com>
2019-08-20 22:13:22 -0400

    Remove unused dependencies.  Remove NSLog statements

--- lib/background_geolocation_firebase.dart
+++ lib/background_geolocation_firebase.dart
@@ -1,8 +1,5 @@
 import 'dart:async';
-import 'dart:ui';
-
 import 'package:flutter/services.dart';
-import 'package:flutter/material.dart';
 
 const _PLUGIN_PATH =
     "com.transistorsoft/flutter_background_geolocation_firebase";
